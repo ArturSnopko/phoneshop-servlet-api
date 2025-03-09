4aa01632a1dcba2b4f322867a8fb1519acfc72ef
Artur <752art71tur57@gmail.com>
2025-03-09 19:41:08 +0300

    Task 1.5: Implement ProductDao

--- src/main/java/com/es/phoneshop/model/product/Product.java
+++ src/main/java/com/es/phoneshop/model/product/Product.java
@@ -17,8 +17,8 @@ public class Product {
     public Product() {
     }
 
-    public Product(Long id, String code, String description, BigDecimal price, Currency currency, int stock, String imageUrl) {
-        this.id = id;
+    public Product( String code, String description, BigDecimal price, Currency currency, int stock, String imageUrl) {
+        //this.id = -1L;
         this.code = code;
         this.description = description;
         this.price = price;
